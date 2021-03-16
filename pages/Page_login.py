c3da6c039e6589027a1649db09549e0d877c0a16
haunvc7049 <haunvc7049@co-well.com.vn>
2021-03-16 15:03:17 +0700

    master

---
+++ pages/Page_login.py
@@ -0,0 +1,1 @@
+def txt_username():    return "//input[@name = 'username']"def txt_password():    return "//input[@name = 'password']"def bnt_login():    return "//button[@type = 'submit']"def msg_result():    return "//div[@id = 'flash-messages']/div"
