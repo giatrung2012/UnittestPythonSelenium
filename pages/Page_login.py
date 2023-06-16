7dbd125e8b703be34cc52b0a1923fc6a75dd02ae
giatrung2012 <trunghsg2012@gmail.com>
2023-06-16 15:39:16 +0700

    Update

--- pages/Page_login.py
+++ pages/Page_login.py
@@ -1,1 +1,1 @@
-def txt_username():    return "//input[@name = 'username']"def txt_password():    return "//input[@name = 'password']"def bnt_login():    return "//button[@type = 'submit']"def msg_result():    return "//div[@id = 'flash-messages']/div"
+def txt_username():    return "//input[@name = 'username']"def txt_password():    return "//input[@name = 'password']"def btn_login():    return "//button[@type = 'submit']"def msg_result():    return "//div[@id = 'flash-messages']/div"
